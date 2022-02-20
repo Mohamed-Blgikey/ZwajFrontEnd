083986eaf293832ede9e90daf0c1c75990576994
Mohamed Blgikey <MohamedAbdellatif@forttech.com>
2022-02-20 20:05:09 +0200

    EditRegister

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { AlertifyService } from '../_Services/alertify.service';
 import { AuthService } from '../_Services/auth.service';
 
@@ -11,15 +12,38 @@ export class RegisterComponent implements OnInit {
 
   model:any={};
   @Output() cancelRegister:EventEmitter<boolean> = new EventEmitter();
-  constructor(private auth:AuthService,private alert:AlertifyService) { }
+
+  registerForm:FormGroup|any;
+
+  constructor(private auth:AuthService, private alert:AlertifyService ,private fb:FormBuilder) { }
 
   ngOnInit(): void {
+    this.createRegisterForm();
   }
+
+  createRegisterForm(){
+    this.registerForm = this.fb.group({
+      gender:['رجل'],
+      email: ['', [Validators.required,Validators.email]],
+      name : ['',Validators.required],
+      dateOfBirth:[null,Validators.required],
+      city:['',Validators.required],
+      country:['',Validators.required],
+      interests:['',Validators.required],
+      introduction:['',Validators.required],
+      lookingFor:['',Validators.required],
+      password: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]],
+     })
+  }
+
+
+
 
   register(){
     // console.log('تم الاشتراك');
     // console.log(this.model);
-    this.auth.register(this.model).subscribe((res:any)=>{
+    console.log(this.registerForm.value);
+    this.auth.register(this.registerForm.value).subscribe((res:any)=>{
       if(res.message == 'تم تسجيل مستخدم جديد'){
         // console.log(res.message);
         this.alert.success(res.message)
@@ -29,6 +53,8 @@ export class RegisterComponent implements OnInit {
         this.alert.error(res.message)
       }
     })
+
+    ;
   }
 
   cancel(){
